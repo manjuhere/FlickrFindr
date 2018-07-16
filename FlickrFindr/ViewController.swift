a825d14b7c5b659f50921a3fa651f7ab3f33b645
Manjunath Chandrashekar <mbond127@gmail.com>
2018-07-16 19:43:41 +0530

    - Project setup. - First commit - LaunchScreen done - Basic UI & Idea laid out

--- FlickrFindr/ViewController.swift
+++ FlickrFindr/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var searchBar: UISearchBar!
+    let searchBtn = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.searchBar.delegate = self
+        self.searchBtn.action = #selector(self.showSearchBar)
+        
+        self.resetNavBar(title: "FlickrFindr")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func showSearchBar() {
+        self.navigationItem.titleView = self.searchBar
+        self.navigationItem.title = nil
+        self.navigationItem.rightBarButtonItem = nil
+        
+        // show recent searches
+        // show a tableview under the navbar for half the screen size.
+        // this can be a containerVC which can be added and removed
+        // VC within containerVC holds the tableview.
+        // this class is a delegate of VC and when didSelectRowAtIndexPath of tableview
+        // pass on to this VC via delegate : fire the API in here, remove containerVC & reload collectionView
     }
-
-
+    
+    @objc func resetNavBar(title: String?) {
+        self.navigationItem.titleView = nil
+        self.navigationItem.title = title
+        self.navigationItem.rightBarButtonItem = searchBtn
+    }
+    
 }
 
+extension ViewController : UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.resetNavBar(title: "FlickrFindr")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.resetNavBar(title: searchBar.text)
+        // fire the api
+        // add searchTerm to recently used
+        // remove container view
+        // reload collection view
+    }
+}
