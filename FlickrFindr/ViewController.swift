69b06745d222d1977e2338c0858f51a9d4d43978
Manjunath Chandrashekar <mbond127@gmail.com>
2018-07-16 18:51:26 +0530

    Initial Commit

---
+++ FlickrFindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrFindr
+//
+//  Created by Manjunath Chandrashekar on 16/07/18.
+//  Copyright © 2018 Manjunath Chandrashekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
